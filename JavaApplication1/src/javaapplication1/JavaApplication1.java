4f8f93d568194fbc4e5deb58cb38602b9c926163
bekkiviau <bekkiviau@25.16.3.254>
2016-03-01 21:06:54 -0500

    Initial Java Application1

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author bekkiviau
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        say("This is a test repo");// TODO code application logic here
+    }
+    
+    public static void say(String text){
+        System.out.print(text);
+    }
+    
+}
